97cb9b55098d6e639dfc07d4fdb6434ebe2ade1e
Yinan Zhu <zynsteve@tamu.edu>
2017-08-17 16:39:16 -0400

    binary tree level order traversal

---
+++ src/breadthFirstSearch/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,49 @@
+package breadthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+	public int val;
+	public TreeNode left, right;
+	public TreeNode(int val) {
+		this.val = val;
+		this.left = this.right = null;
+	}
+}
+ 
+public class BinaryTreeLevelOrderTraversal {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Level order a list of lists of integer
+     */
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+    	ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(level);
+        }
+        
+        return result;
+    }
+}
