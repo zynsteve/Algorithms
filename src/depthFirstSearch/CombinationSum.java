daff28b9fe7db56bd1c088453f913a622ed39b27
Yinan Zhu <zynsteve@tamu.edu>
2017-08-23 15:20:18 -0400

    combination sum

---
+++ src/depthFirstSearch/CombinationSum.java
@@ -0,0 +1,61 @@
+package depthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**Given a set of candidate numbers (C) and a target number (T),
+ * find all unique combinations in C where the candidate numbers sums to T.
+ * The same repeated number may be chosen from C unlimited number of times.
+ * All numbers (including target) will be positive integers.
+ * Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+ * The solution set must not contain duplicate combinations.
+ * 
+ * Example
+ * Given candidate set [2,3,6,7] and target 7, a solution set is:
+ * [
+ *   [7],
+ *   [2, 2, 3]
+ * ]
+ */
+
+public class CombinationSum {
+    /**
+     * @param candidates: A list of integers
+     * @param target:An integer
+     * @return: A list of lists of integers
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (candidates == null) {
+            return result;
+        }
+
+        List<Integer> combination = new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(candidates, 0, target, combination, result);
+
+        return result;
+    }
+
+    public void helper(int[] candidates, int index, int target, List<Integer> combination, List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<Integer>(combination));
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+
+            if (i != index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            combination.add(candidates[i]);
+            helper(candidates, i, target - candidates[i], combination, result);
+            combination.remove(combination.size() - 1);
+        }
+    }
+}
