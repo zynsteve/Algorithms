bd5be02ac0fe3dd9604bad642067e30d18d5549e
Yinan Zhu <zynsteve@tamu.edu>
2017-08-27 21:49:15 -0500

    unique paths

---
+++ src/dynamicProgramming/UniquePaths.java
@@ -0,0 +1,37 @@
+package dynamicProgramming;
+
+/**
+ * A robot is located at the top-left corner of a m x n grid.
+ * The robot can only move either down or right at any point in time.
+ * The robot is trying to reach the bottom-right corner of the grid.
+ * 
+ * Example
+ * Given m = 3 and n = 3, return 6.
+ * Given m = 4 and n = 5, return 35.
+ */
+
+public class UniquePaths {
+    /**
+     * @param n, m: positive integer (1 <= n ,m <= 100)
+     * @return an integer
+     */
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 1;
+        }
+        
+        int[][] sum = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            sum[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            sum[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1];
+            }
+        }
+        return sum[m - 1][n - 1];
+    }
+}
