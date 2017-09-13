278dee40be768ba8aeb2db74de69578bb9e5bf40
Yinan Zhu <zynsteve@tamu.edu>
2017-09-13 11:23:41 -0500

    maximum subarray

---
+++ src/linkedListAndArray/MaximumSubarray.java
@@ -0,0 +1,30 @@
+package linkedListAndArray;
+
+/**
+ * Given an array of integers, find a contiguous subarray which has the largest sum.
+ * 
+ * Example
+ * Given the array [−2,2,−3,4,−1,2,1,−5,3],
+ * the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+ */
+
+public class MaximumSubarray {
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        
+        int max = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            sum = Math.max(sum, 0);
+        }
+
+        return max;
+    }
+}
